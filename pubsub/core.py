08286f2f84a083b5e1976ed6cdb222b02ad0d341
Nick <pacemkr@gmail.com>
2011-12-29 23:25:33 -0500

    Initial.

---
+++ pubsub/core.py
@@ -0,0 +1,108 @@
+import threading
+import time
+import zmq
+from saferef import safeRef
+
+# Create a fowarder in a separate thread with two ends, pub and sub.
+# This way we can just 'connect' to this device whenever we need to subscribe to or send a signal.
+# see: http://lists.zeromq.org/pipermail/zeromq-dev/2010-July/004411.html
+# see: http://lists.zeromq.org/pipermail/zeromq-dev/2010-July/004399.html
+class ForwarderDevice(threading.Thread):
+    daemon = True
+    #in_address = 'inproc://fwdin'
+    #out_address = 'inproc://fwdout'
+    in_address = 'tcp://127.0.0.1:5555'
+    out_address = 'tcp://127.0.0.1:5556'
+
+    def __init__(self, context=None):
+        super(ForwarderDevice, self).__init__()
+        self.context = context or zmq.Context.instance()
+
+    def run(self):
+        insock = self.context.socket(zmq.SUB)
+        insock.bind(self.in_address)
+        insock.setsockopt(zmq.SUBSCRIBE, '')
+
+        outsock = self.context.socket(zmq.PUB)
+        outsock.bind(self.out_address)
+
+        zmq.device(zmq.FORWARDER, insock, outsock)
+
+fwd = ForwarderDevice()
+fwd.start()
+time.sleep(1)
+
+class PubSubHub():
+    _hubs_for_threads = {}
+
+    @staticmethod
+    def instance():
+        ident = threading.current_thread().ident
+        assert ident is not None
+
+        hubs = PubSubHub._hubs_for_threads
+        if ident not in hubs:
+            hubs[ident] = PubSubHub()
+
+        return hubs[ident]
+
+    def __init__(self, context=None):
+        context = context or zmq.Context.instance()
+
+        self._out_socket = context.socket(zmq.PUB)
+        self._out_socket.connect(ForwarderDevice.in_address)
+
+        self._in_socket = context.socket(zmq.SUB)
+        self._in_socket.connect(ForwarderDevice.out_address)
+        self._in_socket.setsockopt(zmq.SUBSCRIBE, '')
+
+        self._receivers = {}
+
+    def send(self, topic, data):
+        self._out_socket.send_json([topic, data])
+
+    def get_fd(self):
+        return self._in_socket.FD
+
+    def receive_messages(self):
+        try:
+            while True:
+                topic, data = self._in_socket.recv_json(flags=zmq.NOBLOCK)
+                assert isinstance(data, dict)
+
+                for receiver_ref in self._receivers[topic]:
+                    receiver_ref()(**data)
+
+        except zmq.ZMQError, ex:
+            if not ex.errno == zmq.EAGAIN: raise ex
+
+    def connect_receiver(self, topic, receiver):
+        assert callable(receiver)
+
+        if topic not in self._receivers:
+            self._receivers[topic] = []
+
+        self._receivers[topic].append(safeRef(receiver, self._receiver_deleted))
+
+    def _receiver_deleted(self, receiver_ref):
+        for topic_receivers in self._receivers.itervalues():
+            topic_receivers.remove(receiver_ref)
+
+
+def receiver(message):
+    assert isinstance(message, Message)
+
+    def receiver_decorator(func):
+        hub = PubSubHub.instance()
+        hub.connect_receiver(message.topic, func)
+        return func
+
+    return receiver_decorator
+
+class Message:
+    def __init__(self, topic):
+        self.topic = topic
+
+    def send(self, **kwargs):
+        hub = PubSubHub.instance()
+        hub.send(self.topic, data=kwargs)
---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+import os
+
+DESCRIPTION = "Thread safe PubSub component with Django Signals like API. Built using ØMQ (zeromq) sockets."
+
+LONG_DESCRIPTION = None
+try:
+    LONG_DESCRIPTION = open('README.rst').read()
+except:
+    pass
+
+
+setup(name='pubsub',
+      version='0.1.0',
+      packages=find_packages(),
+      author='Nick Zalutskiy',
+      author_email='pacemkr@{nospam}gmail.com',
+      url='https://github.com/pacemkr/pubsub',
+      include_package_data=True,
+      description=DESCRIPTION,
+      long_description=LONG_DESCRIPTION,
+      platforms=['any'],
+)
