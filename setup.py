a0871baa7fe92bdaa47a81915943deab6575aa15
Nick Zalutskiy <pacemkr@gmail.com>
2012-02-17 20:35:49 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,6 @@
 from setuptools import setup, find_packages
 import os
 
-DESCRIPTION = "Thread safe PubSub component with Qt Signals like API. Built using ØMQ (zeromq) sockets."
-
-LONG_DESCRIPTION = None
-try:
-    LONG_DESCRIPTION = open('README.rst').read()
-except:
-    pass
-
-
 setup(name='pubsub',
       version='0.1.0',
       packages=find_packages(),
@@ -17,7 +8,6 @@ setup(name='pubsub',
       author_email='pacemkr@{nospam}gmail.com',
       url='https://github.com/pacemkr/pubsub',
       include_package_data=True,
-      description=DESCRIPTION,
-      long_description=LONG_DESCRIPTION,
+      description="Thread safe PubSub component with Qt Signals like API. Built using ØMQ (zeromq) sockets.",
       platforms=['any'],
 )
