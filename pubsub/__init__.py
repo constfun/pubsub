3b9f686b4cd894dc74e8fb993d5c9076efe0a54b
Nick <pacemkr@gmail.com>
2012-06-11 15:20:16 -0400

    Improving forwarder device a bit. Not requiring rabbitmq or zeromq in __init__.

--- pubsub/__init__.py
+++
@@ -1,3 +0,0 @@
-from core import Message
-from zmqhub import ZmqHub
-from rabbithub import RabbitHub
--- pubsub/zmqhub.py
+++ pubsub/zmqhub.py
@@ -2,28 +2,35 @@ from threading import Thread
 import zmq
 from core import Hub
 
+
 # Create a fowarder in a separate thread with two ends, pub and sub.
 # This way we can just 'connect' to this device whenever we need to subscribe to or send a signal.
 # see: http://lists.zeromq.org/pipermail/zeromq-dev/2010-July/004411.html
 # see: http://lists.zeromq.org/pipermail/zeromq-dev/2010-July/004399.html
 class ForwarderDevice(Thread):
     daemon = True
-    #in_address = 'inproc://fwdin'
-    #out_address = 'inproc://fwdout'
+
     in_address = 'tcp://127.0.0.1:5555'
     out_address = 'tcp://127.0.0.1:5556'
 
     def run(self):
         context = zmq.Context.instance()
 
-        insock = context.socket(zmq.SUB)
-        insock.bind(self.in_address)
-        insock.setsockopt(zmq.SUBSCRIBE, '')
+        try:
+            insock = context.socket(zmq.SUB)
+            insock.bind(self.in_address)
+            insock.setsockopt(zmq.SUBSCRIBE, '')
 
-        outsock = context.socket(zmq.PUB)
-        outsock.bind(self.out_address)
+            outsock = context.socket(zmq.PUB)
+            outsock.bind(self.out_address)
 
-        zmq.device(zmq.FORWARDER, insock, outsock)
+            zmq.device(zmq.FORWARDER, insock, outsock)
+        except zmq.ZMQError as ex:
+            # errno 48 means failed to bind.
+            # Probably means the forwarder is already running.
+            if not ex.errno == 48:
+                raise ex
+
 
 class ZmqHub(Hub):
     _forwarder = None
@@ -53,7 +60,8 @@ class ZmqHub(Hub):
                 topic, payload = self._in_socket.recv_json(flags=zmq.NOBLOCK)
                 self.Rx(topic=topic, payload=payload)
         except zmq.ZMQError, ex:
-            if not ex.errno == zmq.EAGAIN: raise ex
+            if not ex.errno == zmq.EAGAIN:
+                raise ex
 
     def fileno(self):
         return self._in_socket.FD
